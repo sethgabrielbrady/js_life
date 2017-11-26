f042a6d874b990b13ec7e20149c8b7afa16a4ab9
Seth Brady <sethgabrielbrady@gmail.com>
2017-11-25 19:38:52 -0500

    update to movement

--- life2.js
+++ life2.js
@@ -1,60 +1,65 @@
 let ball = document.getElementById("ball");
 let moveDist = 20;
-var xPos = 0;
-var yPos = 0;
+let viewportOffset = ball.getBoundingClientRect();
+let ballTop = viewportOffset.top;
+let ballLeft = viewportOffset.left;
+let w = window.innerWidth;
+let h = window.innerHeight;
+let moveSpeed = 100;
+let rand = function(){
+  return Math.floor((Math.random() * 4) + 1);
+};
 
-var startFunction = function(){
-  let rand = Math.floor((Math.random() * 4) + 1);
-  let x = xPos;
-  let y = yPos;
+let startFunction = function(){
 
-  if (rand === 1){
-    yPos = y + moveDist;
-    ball.setAttribute("style","left:" + (y + moveDist)+"px;");
-  }else if (rand === 2){
-    yPos = y - moveDist;
-    ball.setAttribute("style","left:" + (y - moveDist) +"px;");
-  }else if (rand === 3){
-    xPos = x - moveDist;
-    ball.setAttribute("style","top:" + (x - moveDist) +"px;");
-  }else if (rand === 4){
-    xPos = x + moveDist;
-    ball.setAttribute("style","top:"+ (x + moveDist) +"px;");
-  }
+  setInterval(function(){
+    console.log("x:", + ballTop + "y:" + ballLeft);
+    rand();
+
+    if (rand() === 1){
+      console.log("left");
+      ballLeft = ballLeft - moveDist;
+      if(ballLeft <= 0){
+        rand();
+      } else {
+        ball.setAttribute("style","left:" + (ballLeft) + "px; top:"+ ballTop+"px;");
+      }
+    }else if (rand() === 2){
+      console.log("right");
+      ballLeft = ballLeft + moveDist;
+      if(ballLeft >= w){
+        rand();
+      } else {
+        ball.setAttribute("style","left:" + (ballLeft) + "px; top:"+ ballTop+"px;");
+      }
+    }else if (rand() === 3){
+      console.log("down");
+      ballTop = ballTop + moveDist;
+      if(ballTop >= h){
+        rand();
+      } else {
+        ball.setAttribute("style","top:" + (ballTop) + "px; left:"+ ballLeft+"px;");
+      }
+    }else if (rand() === 4){
+      console.log("up");
+      ballTop = ballTop - moveDist;
+      if(ballTop <= 0){
+        rand();
+      } else {
+        ball.setAttribute("style","top:"+ (ballTop) + "px; left:"+ ballLeft+"px;");
+      }
+    }
+  }, moveSpeed);
 };
 
 
-// To DO
-
+// Step 1 -COMPLETE
 // : Get the current coordinates of the ball.
 // : Move the ball into a random spot based on the its current position
 // : Do this every 2 seconds
 
-
-// Helper function to get an element's exact position
-function getPosition(el) {
-  while (el) {
-    if (el.tagName == "BODY") {
-      // deal with browser quirks with body/window/document and page scroll
-      var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
-      var yScroll = el.scrollTop || document.documentElement.scrollTop;
-
-      xPos += (el.offsetLeft - xScroll + el.clientLeft);
-      yPos += (el.offsetTop - yScroll + el.clientTop);
-    } else {
-      // for all other non-BODY elements
-      xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
-      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
-    }
-
-    el = el.offsetParent;
-  }
-  return {
-    x: xPos,
-    y: yPos
-  };
-}
-
-// deal with the page getting resized or scrolled
-// window.addEventListener("scroll", updatePosition, false);
-// window.addEventListener("resize", updatePosition, false);
+// Step 2
+// Notes- the bugs should be turned into objects with constructor
+// : Add boundaeries
+// : Add the ability for multiple "bugs"
+// : Add hit detection on the bugs
