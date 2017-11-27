a2cd9442815ca0ee7a46f6df6791fd044033226c
Seth Brady <sethgabrielbrady@gmail.com>
2017-11-27 09:48:30 -0500

    updated

--- life2.js
+++ life2.js
@@ -1,13 +1,13 @@
 const MoveDist = 10;
-const MoveSpeed = 100;
+const MoveSpeed = 200;
 
-let ball = document.getElementById("ball");
+let bug = document.getElementById("bug");
 let yPos = document.getElementById("yPos");
 let xPos = document.getElementById("xPos");
 let windowDim = document.getElementById("windowDim");
-let viewportOffset = ball.getBoundingClientRect();
-let ballTop = viewportOffset.top;
-let ballLeft = viewportOffset.left;
+let viewportOffset = bug.getBoundingClientRect();
+let bugTop = viewportOffset.top;
+let bugLeft = viewportOffset.left;
 let w = window.innerWidth;
 let h = window.innerHeight;
 
@@ -17,60 +17,56 @@ let startFunction = function(){
 
   setInterval(function(){
     let rand = Math.floor((Math.random() * 4) + 1);
-    yPos.innerHTML = "Y pos: " + ballLeft;
-    xPos.innerHTML = "X pos: " + ballTop;
+    yPos.innerHTML = "Y pos: " + bugLeft;
+    xPos.innerHTML = "X pos: " + bugTop;
 
     //will stop everything if the bug manages to go beyond the boundaries
-    if (ballLeft >= w || ballLeft <= 0){
+    if (bugLeft >= w || bugLeft <= 0){
       return 0;
     }
-    if (ballTop >= h || ballTop <= 0){
+    if (bugTop >= h || bugTop <= 0){
       return 0;
     }
 
+    //convert this to a switch statement
     if (rand === 1){
-      if (ballLeft <= 50){
-        ballLeft = ballLeft + MoveDist;
+      if (bugLeft <= 50){
+        bugLeft = bugLeft + MoveDist;
       }else{
-        ballLeft = ballLeft - MoveDist;
+        bugLeft = bugLeft - MoveDist;
       }
-      ball.setAttribute("style","left:" + (ballLeft) + "px; top:"+ ballTop+"px; border-left:4px solid yellow;");
+      bug.setAttribute("style","left:" + (bugLeft) + "px; top:"+ bugTop+"px; border-left:4px solid yellow;");
     }else if (rand === 2){
-      if (ballLeft >= w - 50){
-        ballLeft = ballLeft - MoveDist;
+      if (bugLeft >= w - 50){
+        bugLeft = bugLeft - MoveDist;
       }else{
-        ballLeft = ballLeft + MoveDist;
+        bugLeft = bugLeft + MoveDist;
       }
-      ball.setAttribute("style","left:" + (ballLeft) + "px; top:"+ ballTop+"px; border-right:4px solid yellow;");
+      bug.setAttribute("style","left:" + (bugLeft) + "px; top:"+ bugTop+"px; border-right:4px solid yellow;");
     }else if (rand === 3){
-      if (ballTop >= h - 50){
-        ballTop = ballTop - MoveDist;
+      if (bugTop >= h - 50){
+        bugTop = bugTop - MoveDist;
       }else {
-        ballTop = ballTop + MoveDist;
+        bugTop = bugTop + MoveDist;
       }
-      ball.setAttribute("style","top:" + (ballTop) + "px; left:"+ ballLeft+"px; border-bottom:4px solid yellow;");
+      bug.setAttribute("style","top:" + (bugTop) + "px; left:"+ bugLeft+"px; border-bottom:4px solid yellow;");
     }else if (rand === 4){
-      if (ballTop <= 50){
-        ballTop = ballTop + MoveDist;
+      if (bugTop <= 50){
+        bugTop = bugTop + MoveDist;
       }else{
-        ballTop = ballTop - MoveDist;
+        bugTop = bugTop - MoveDist;
       }
-      ball.setAttribute("style","top:"+ (ballTop) + "px; left:"+ ballLeft+"px; border-top:4px solid yellow;");
+      bug.setAttribute("style","top:"+ (bugTop) + "px; left:"+ bugLeft+"px; border-top:4px solid yellow;");
     }
   }, MoveSpeed);
 };
 
 
-// Step 1 -COMPLETE
-// (X) Get the current coordinates of the ball.
-// (X) Move the ball into a random spot based on the its current position
-// (X) Do this every 2 seconds
-
 // Step 2
 // Notes- the bugs should be turned into objects with constructor
-// (X) Add boundaeries
+
 // (:) Add "food" and a sense for the bug that increases it chances of finding it
-// (:) If the bug doesnt find it in an alloted time, it dies.
+// (:) Add a life timer to the bug. Food will increase the life(time) of the bug.
 
 // Step 3
 // ADding random elements that affect time, move speed, etc.
