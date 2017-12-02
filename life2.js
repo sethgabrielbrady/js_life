c305f85d420c965ad07eb95076e9d6212d09683e
Seth Brady <sethgabrielbrady@gmail.com>
2017-12-02 11:39:29 -0500

    put bugg movement insise a seperate fn

--- life2.js
+++ life2.js
@@ -1,13 +1,14 @@
 const MoveDist = 10;
 const BaseSpeed = 200;
 
+// let bugg = document.getElementsByClassName("bugg");
 let bugg = document.getElementById("bugg");
 let yPos = document.getElementById("yPos");
 let xPos = document.getElementById("xPos");
 let windowDim = document.getElementById("windowDim");
-let viewportOffset = bugg.getBoundingClientRect();
-let buggXPos = viewportOffset.top;
-let buggYPos = viewportOffset.left;
+// let viewportOffset = bugg.getBoundingClientRect();
+// let buggXPos = viewportOffset.top;
+// let buggYPos = viewportOffset.left;
 let x = window.innerWidth;
 let y = window.innerHeight;
 let buggCount = 0;
@@ -24,16 +25,11 @@ function Bugg (name){
   let rngX;
   let rngy;
 
-  if (buggyCount > 1 ||  buggyCount < 1){
-    console.log("Youve already made this bug");
-  }else{
-    rngX = rngControl(x);
-    rngY = rngControl(y);
-    rngSpeedModifier = rngControl(10);
-    buggCount = buggCount + 1;
-    // bugg.setAttribute("style", "top:"+rngY+"; left:"+rngX+"; background-color: green;");
-  }
-
+  rngX = rngControl(x);
+  rngY = rngControl(y);
+  rngSpeedModifier = rngControl(10);
+  buggCount = buggCount + 1;
+  console.log(buggCount);
   this.name = name;
   this.number = buggCount;
   this.buggSpeed = BaseSpeed * (rngSpeedModifier/10);
@@ -42,81 +38,88 @@ function Bugg (name){
   console.log(this.stats);
 }
 
+new Bugg(1);
+new Bugg(2);
+new Bugg(3);
+
 //RNG Controller
 function rngControl(number){
   let newNum = Math.floor((Math.random() * number) + 1);
   return newNum;
 }
 
+//Controls the random movement
+function buggMovement(){
+  let moveSpeed = BaseSpeed;
+  //set the random position inside start
+  buggYPos = rngControl(x);
+  buggXPos= rngControl(y);
+  console.log(buggXPos + ':' + buggYPos);
+
+  return setInterval(function(){
+    let rngMove = rngControl(4);//keep this inside the setInterval function.
+
+    yPos.innerHTML = "Y pos: " + buggYPos;
+    xPos.innerHTML = "X pos: " + buggXPos;
+
+    //will stop everything if the bugg manages to go beyond the boundaries
+    //may want to expand this and move into a fucntion
+    if (buggYPos >= x || buggYPos <= 0){
+      return 0;
+    }
+    if (buggXPos >= y || buggXPos <= 0){
+      return 0;
+    }
+    //convert this to a switch statement
+    if (rngMove === 1){
+      if (buggYPos <= 50){
+        buggYPos = buggYPos + MoveDist;
+      }else{
+        buggYPos = buggYPos - MoveDist;
+      }
+      bugg.setAttribute("style","left:" + (buggYPos) + "px; top:"+ buggXPos+"px; border-left:4px solid yellow;");
+    }else if (rngMove === 2){
+      if (buggYPos >= x - 50){
+        buggYPos = buggYPos - MoveDist;
+      }else{
+        buggYPos = buggYPos + MoveDist;
+      }
+      bugg.setAttribute("style","left:" + (buggYPos) + "px; top:"+ buggXPos+"px; border-right:4px solid yellow;");
+    }else if (rngMove === 3){
+      if (buggXPos >= y - 50){
+        buggXPos = buggXPos - MoveDist;
+      }else {
+        buggXPos = buggXPos + MoveDist;
+      }
+      bugg.setAttribute("style","top:" + (buggXPos) + "px; left:"+ buggYPos+"px; border-bottom:4px solid yellow;");
+    }else if (rngMove === 4){
+      if (buggXPos <= 50){
+        buggXPos = buggXPos + MoveDist;
+      }else{
+        buggXPos = buggXPos - MoveDist;
+      }
+      bugg.setAttribute("style","top:"+ (buggXPos) + "px; left:"+ buggYPos+"px; border-top:4px solid yellow;");
+    }
+
+  }, moveSpeed);
+}
+
 //Start Function
-function startFunction(){
+function startFunction(buggLimit){
   startCount = startCount + 1;
 
   //BUGG movement and starting position.
   if (startCount > 1 ||  startCount <1){
     console.log("You've already started.");
   }else{
-    let moveSpeed = BaseSpeed;
-    //set the random position inside start
-    buggYPos = rngControl(x);
-    buggXPos= rngControl(y);
-    console.log(buggXPos + ':' + buggYPos);
-
-    setInterval(function(){
-
-      let rngMove = rngControl(4);
-
-      yPos.innerHTML = "Y pos: " + buggYPos;
-      xPos.innerHTML = "X pos: " + buggXPos;
-
-      //will stop everything if the bugg manages to go beyond the boundaries
-      //may want to expand this and move into a fucntion
-      if (buggYPos >= x || buggYPos <= 0){
-        return 0;
-      }
-      if (buggXPos >= y || buggXPos <= 0){
-        return 0;
-      }
-      //convert this to a switch statement
-      if (rngMove === 1){
-        if (buggYPos <= 50){
-          buggYPos = buggYPos + MoveDist;
-        }else{
-          buggYPos = buggYPos - MoveDist;
-        }
-        bugg.setAttribute("style","left:" + (buggYPos) + "px; top:"+ buggXPos+"px; border-left:4px solid yellow;");
-      }else if (rngMove === 2){
-        if (buggYPos >= x - 50){
-          buggYPos = buggYPos - MoveDist;
-        }else{
-          buggYPos = buggYPos + MoveDist;
-        }
-        bugg.setAttribute("style","left:" + (buggYPos) + "px; top:"+ buggXPos+"px; border-right:4px solid yellow;");
-      }else if (rngMove === 3){
-        if (buggXPos >= y - 50){
-          buggXPos = buggXPos - MoveDist;
-        }else {
-          buggXPos = buggXPos + MoveDist;
-        }
-        bugg.setAttribute("style","top:" + (buggXPos) + "px; left:"+ buggYPos+"px; border-bottom:4px solid yellow;");
-      }else if (rngMove === 4){
-        if (buggXPos <= 50){
-          buggXPos = buggXPos + MoveDist;
-        }else{
-          buggXPos = buggXPos - MoveDist;
-        }
-        bugg.setAttribute("style","top:"+ (buggXPos) + "px; left:"+ buggYPos+"px; border-top:4px solid yellow;");
-      }
-
-    }, moveSpeed);
+    buggMovement();
   }
-
 }
 
-
+//Buggmovement needs to be called for each instance of bugg. Maybe call it inside the constructor or seperate function that
 
 //The startFucntion should call the bugg Constructor (making a new bug).
-//Bugg movement should be moved to the Bugg Constructor. 
+//Bugg movement should be moved to the Bugg Constructor.
 //
 //
 //
